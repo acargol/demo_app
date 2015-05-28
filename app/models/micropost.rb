a510ad547d23b7f65cae9e28f7de2870cf9523ac
Austin Cargol <acargol@square-root.com>
2015-05-28 14:44:22 -0500

    hound 4

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,10 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   
   validates :content, :length => {:maximuim => 140}
+
+    private
+
+  def test_method
+    puts "does something"
+  end
 end
